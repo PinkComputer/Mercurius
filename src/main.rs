fa99a50d3d81524ac487d0690ad5544e64a1ac26
PinkComputer <106210979+PinkComputer@users.noreply.github.com>
2025-05-31 20:56:56 -0400

    Updated to actually have the local git repo working :3

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,14 @@ use std::{
     str::FromStr,
     str,
     io,
-    env,
+    //env,
     net::Shutdown,
     thread,
 };
 
 // use chrono::{NaiveDate, NaiveDateTime};
+
+use hex
 
 
 //At the time I was unaware there was already an IP address type and I was too prideful to switch
@@ -48,15 +50,62 @@ impl FromStr for Ipv4 {
         let u8_4_fromstr: u8 = iter.next().unwrap().parse().unwrap();
 
         Ok(Ipv4(u8_1_fromstr,u8_2_fromstr,u8_3_fromstr,u8_4_fromstr))
-        
+     
 
     }
 }
- 
+
+// May God strike you down FP.
+#[derive(Clone)]
+struct Ipv6 (u16,u16,u16,u16,u16,u16,u16,u16);
+
+impl fmt::Display for Ipv6 {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({:x}:{:x}:{:x}:{:x}:{:x}:{:x}:{:x}:{:x})", self.0, self.1, self.2, self.3, self.4, self.5, self.6, self.7)
+    }
+}
+
+
+#[derive(Debug, PartialEq, Eq)]
+struct Ipv6ParseError; 
+impl fmt::Display for Ipv6ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invaild address: {}", (format!("{}", self)))
+    }
+}
+
+impl FromStr for Ipv6 {
+    type Err = Ipv6ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut iter = s
+            .strip_prefix('(')
+            .and_then(|s| s.strip_suffix(')'))
+            .and_then(|s| Some(s.split(':')))
+            .ok_or(Ipv6ParseError)?;
+        //The iterator is started/moved, unwrapped into a string, parsed into u16, and then unwrapped
+        //again. Is this a good way to do it? Prolly not
+        let u16_1_fromstr: u16 = iter.next().unwrap().parse().unwrap(); 
+        let u16_2_fromstr: u16 = iter.next().unwrap().parse().unwrap();
+        let u16_3_fromstr: u16 = iter.next().unwrap().parse().unwrap();
+        let u16_4_fromstr: u16 = iter.next().unwrap().parse().unwrap();
+        let u16_5_fromstr: u16 = iter.next().unwrap().parse().unwrap(); 
+        let u16_6_fromstr: u16 = iter.next().unwrap().parse().unwrap();
+        let u16_7_fromstr: u16 = iter.next().unwrap().parse().unwrap();
+        let u16_8_fromstr: u16 = iter.next().unwrap().parse().unwrap();
+
+
+        Ok(Ipv6(u16_1_fromstr,u16_2_fromstr,u16_3_fromstr,u16_4_fromstr,u16_5_fromstr,u16_6_fromstr,u16_7_fromstr,u16_8_fromstr))
+     
+
+    }
+}
+
+
+
 
 struct Message {
-    to: Ipv4,
-    from: Ipv4,
+    to: IpAdd,
+    from: IpAdd,
     from_public: u8,
     to_public: u8,
     message: String, 
@@ -112,8 +161,8 @@ impl Message {
        let message_request:String = line_iter.to_string();
 
        let result = Message {
-           to: to_ip_request,
-           from: from_ip_request,
+           to: IpAdd::V4(to_ip_request),
+           from: IpAdd::V4(from_ip_request),
            from_public: from_key_request,
            to_public: to_key_request,
            message: message_request
@@ -124,30 +173,143 @@ impl Message {
 
     }
 }
+#[derive(Clone)]
+enum IpAdd {
+    V4(Ipv4),
+    V6(Ipv6)
+}
+
+impl fmt::Display for IpAdd {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match &self {
+            IpAdd::V4(Ip_v4) => write!(f, "({}.{}.{}.{})", Ip_v4.0, Ip_v4.1, Ip_v4.2, Ip_v4.3),
+            IpAdd::V6(Ip_v6) => write!(f, "({}:{}:{}:{}:{}:{}:{}:{})", Ip_v6.0, Ip_v6.1, Ip_v6.2, Ip_v6.3, Ip_v6.4, Ip_v6.5, Ip_v6.6, Ip_v6.7)
+        }
+    }
+}
 
 #[derive(Clone)]
 struct FullIp {
-    address: Ipv4,
+    address: IpAdd,
     port: u16,
 }
 
+#[derive(Debug, PartialEq, Eq)]
+struct FullIpParseError; 
+impl fmt::Display for FullIpParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Invaild address: {}", (format!("{}", self)))
+    }
+}
+
+impl FullIp {
+    fn from_str_ipv4(s: &str) -> Result<Self, <FullIp as FromStr>::Err>  {
+        let mut iter = s
+            .strip_prefix('(')
+            .and_then(|s| s.strip_suffix(')'))
+            .and_then(|s| Some(s.split('.')))
+            .ok_or(FullIpParseError)?;
+        //The iterator is started/moved, unwrapped into a string, parsed into u8, and then unwrapped
+        //again. Is this a good way to do it? Prolly not
+
+        if (iter.clone().count() <= 1usize) {
+            return Err(FullIpParseError);
+        }
+
+        let u8_1_fromstr = iter.next().unwrap().parse::<u8>();
+        let u8_2_fromstr = iter.next().unwrap().parse::<u8>();
+        let u8_3_fromstr = iter.next().unwrap().parse::<u8>();
+        let u8_4_fromstr = iter.next().expect("Failed at u8_4").split_once(":").unwrap().0.parse();
+        
+        
+        if (u8_1_fromstr.is_err()) {
+            return Err(FullIpParseError)
+        }
+        
+        let ipadd = IpAdd::V4(Ipv4(u8_1_fromstr.unwrap(),u8_2_fromstr.unwrap(),u8_3_fromstr.unwrap(),u8_4_fromstr.unwrap()));
+        let (_wrong, port) = s
+            .strip_suffix(')')
+            .and_then(|s| s.split_once(":"))
+            .ok_or(FullIpParseError)?;
+        let port_fromstring = port.parse::<u16>().map_err(|_| FullIpParseError)?;
+        
+        Ok(FullIp{address:ipadd, port:port_fromstring})
+        
+
+    }
+}
+
+
+
+impl FullIp {
+
+    fn from_str_ipv6(s: &str) -> Result<Self, <FullIp as FromStr>::Err>  {
+        // Example Ipv6 with port: 
+        // ([2001:0db8:0000:0000:0000:8a2e:0370:7334]:7878)
+        let mut iter = s
+            .strip_prefix('(')
+            .and_then(|s| s.strip_suffix(')'))
+            .and_then(|s| s.strip_prefix('['))
+            .and_then(|s| Some(s.split(':')))
+            .ok_or(FullIpParseError)?;
+        //The iterator is started/moved, unwrapped into a string, parsed into u8, and then unwrapped
+        //again. Is this a good way to do it? Prolly not
+        let u16_1_fromstr = iter.next().unwrap().parse::<u16>(); 
+        let u16_2_fromstr = iter.next().unwrap().parse::<u16>();
+        let u16_3_fromstr = iter.next().unwrap().parse::<u16>();
+        let u16_4_fromstr = iter.next().unwrap().parse::<u16>(); 
+        let u16_5_fromstr = iter.next().unwrap().parse::<u16>();
+        let u16_6_fromstr = iter.next().unwrap().parse::<u16>();
+        let u16_7_fromstr = iter.next().unwrap().parse::<u16>();
+        let u16_8_fromstr = iter.next().expect("Failed at u16_8").strip_suffix(']').unwrap().parse::<u16>();
+        
+        
+        if (u16_1_fromstr.is_err()) {
+            return Err(FullIpParseError)
+        }
+        
+
+        let ipadd = IpAdd::V6(Ipv6(u16_1_fromstr.unwrap(),u16_2_fromstr.unwrap(),u16_3_fromstr.unwrap(),u16_4_fromstr.unwrap(),u16_5_fromstr.unwrap(),u16_6_fromstr.unwrap(),u16_7_fromstr.unwrap(),u16_8_fromstr.unwrap()));
+        let port = s
+            .strip_suffix(')')
+            .and_then(|s| s.split(":").skip(5).next())
+            .ok_or(FullIpParseError)?;
+        let port_fromstring = port.parse::<u16>().map_err(|_| FullIpParseError)?;
+        
+        Ok(FullIp{address:ipadd, port:port_fromstring})
+        
+
+    }
+}
+
+
 
 impl FromStr for FullIp {
-    type Err = Ipv4ParseError;
+    type Err = FullIpParseError;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let ipv4_result = FullIp::from_str_ipv4(s);
+        let full_ip_str = match ipv4_result {
+            Ok(fullip) => fullip,
+            Err(_error) => match FullIp::from_str_ipv6(s) {
+                Ok(ip) => ip,
+                Err(e) => panic!("Cannot create string!: {e:?}")
+            }
+        };
+        Ok(full_ip_str)
+        /*
         let mut iter = s
             .strip_prefix('(')
             .and_then(|s| s.strip_suffix(')'))
             .and_then(|s| Some(s.split('.')))
             .ok_or(Ipv4ParseError)?;
-        //The iterator is started/moved, unwrapped into a string, parsed into u8, and then unwrapped
+    //The iterator is started/moved, unwrapped into a string, parsed into u8, and then unwrapped
         //again. Is this a good way to do it? Prolly not
         let u8_1_fromstr: u8 = iter.next().unwrap().parse().unwrap(); 
         let u8_2_fromstr: u8 = iter.next().unwrap().parse().unwrap();
         let u8_3_fromstr: u8 = iter.next().unwrap().parse().unwrap();
         let u8_4_fromstr: u8 = iter.next().expect("Failed at u8_4").split_once(":").unwrap().0.parse().unwrap();
 
-        let ipadd = Ipv4(u8_1_fromstr,u8_2_fromstr,u8_3_fromstr,u8_4_fromstr);
+        let ipadd = IpAdd(V4(Ipv4(u8_1_fromstr,u8_2_fromstr,u8_3_fromstr,u8_4_fromstr)));
         let (_wrong, port) = s
             .strip_suffix(')')
             .and_then(|s| s.split_once(":"))
@@ -156,13 +318,16 @@ impl FromStr for FullIp {
         
         Ok(FullIp{address:ipadd, port:port_fromstring})
         
-
+        */
     }
 }
 
 impl fmt::Display for FullIp {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "({}.{}.{}.{}:{})", self.address.0, self.address.1, self.address.2, self.address.3, self.port)
+        match &self.address {
+            IpAdd::V4(Ip_v4) => write!(f, "({}.{}.{}.{}:{})", Ip_v4.0, Ip_v4.1, Ip_v4.2, Ip_v4.3, self.port),
+            IpAdd::V6(Ip_v6) => write!(f, "([{}:{}:{}:{}:{}:{}:{}:{}]:{})", Ip_v6.0, Ip_v6.1, Ip_v6.2, Ip_v6.3, Ip_v6.4, Ip_v6.5, Ip_v6.6, Ip_v6.7, self.port)
+        }
     }
 }
 
@@ -173,7 +338,7 @@ impl FullIp {
 }
 
 fn main() {
-    env::set_var("RUST_BACKTRACE", "1");
+    //env::set_var("RUST_BACKTRACE", "1");
     
     println!("   __  ___                     _        
   /  |/  /__ __________ ______(_)_ _____
@@ -184,13 +349,13 @@ fn main() {
     println!("Live chat only");
     
     let mut local_ip = String::new();
-    print!("Enter IP and port you would like connections to go to (ex:127.0.0.1:7878): ");
+    print!("Enter IP and port you would like connections to go to (ex:(127.0.0.1:7878) or ([2001:0db8:0000:0000:0000:8a2e:0370:7334]:7878) ):");
     io::stdout().flush().unwrap();
     io::stdin()
         .read_line(&mut local_ip)
         .expect("Failed to read lines");
     
-
+    
 
     let local_ip: FullIp = FullIp::from_str(local_ip.trim()).expect("Invalid address! Exiting...");
     let mut listener = TcpListener::bind(FullIp::connect_format(&local_ip)).expect("Listener could not be set up...");
@@ -275,7 +440,7 @@ fn stream_writing(mut stream: TcpStream, remote: FullIp) {
 
         let response_message = Message {
             to: remote.address.clone(),
-            from: Ipv4::from_str(&(format!("({})", stream.local_addr().unwrap().ip()))).unwrap(),
+            from: IpAdd::V4(Ipv4::from_str(&(format!("({})", stream.local_addr().unwrap().ip()))).unwrap()),
             from_public: 1,
             to_public: 2,
             message: ent_message,
